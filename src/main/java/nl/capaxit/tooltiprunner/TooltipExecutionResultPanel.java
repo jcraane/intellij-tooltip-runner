1b3b71c83c79a30adcc66dd0dc5a2e7e14086c88
jcraane <jcraane@gmail.com>
2016-08-09 21:52:25 +0200

    Automatic hiding of tooltip.

--- src/main/java/nl/capaxit/tooltiprunner/TooltipExecutionResultPanel.java
+++ src/main/java/nl/capaxit/tooltiprunner/TooltipExecutionResultPanel.java
@@ -5,11 +5,15 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.awt.RelativePoint;
 import com.intellij.ui.components.panels.NonOpaquePanel;
+import com.intellij.util.Alarm;
+import com.intellij.util.ui.Animator;
+import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 import java.awt.*;
@@ -21,10 +25,15 @@ import java.awt.*;
  */
 public class TooltipExecutionResultPanel extends NonOpaquePanel {
     private JBPopup result;
+    public static final int HIDE_DELAY = 4 * 1000;
     private final Project project;
     private final Editor editor;
+    private Alarm hideAlarm = new Alarm();
+    private Animator animator;
     private JPanel resultPanel;
     private final String executionResult;
+    private Phase phase = Phase.FADING_IN;
+    private float hintAlpha = (UIUtil.isUnderDarcula()) ? 0.05F : 0.1F;
 
     public TooltipExecutionResultPanel(final Project project, final String executionResult, final Editor editor) {
         this.project = project;
@@ -44,6 +53,11 @@ public class TooltipExecutionResultPanel extends NonOpaquePanel {
                 .createPopup();
         resultPanel.add(createLabel(executionResult));
         result.show(computeLocation(ideFrame));
+
+        animator = new FadeInOutAnimator(true);
+//        result.show(computeLocation(ideFrame));
+        animator.resume();
+
     }
 
     private RelativePoint computeLocation(IdeFrame ideFrame) {
@@ -75,7 +89,61 @@ public class TooltipExecutionResultPanel extends NonOpaquePanel {
         result.getContent().invalidate();
         final IdeFrame ideFrame = WindowManager.getInstance().getIdeFrame(project);
         result.setLocation(computeLocation(ideFrame).getScreenPoint());
-        result.setSize(getPreferredSize());
+//        result.setSize(getPreferredSize());
         result.getContent().repaint();
+        showFinal();
     }
+
+    private void showFinal() {
+        phase = Phase.SHOWN;
+        setAlpha(hintAlpha);
+        hideAlarm.cancelAllRequests();
+        hideAlarm.addRequest(() -> fadeOut(), HIDE_DELAY);
+    }
+
+    private void setAlpha(float alpha) {
+        Window window = getHintWindow();
+        if (window != null) {
+            WindowManager.getInstance().setAlphaModeRatio(window, alpha);
+        }
+    }
+
+    private void fadeOut() {
+        if (phase != Phase.SHOWN) return;
+        phase = Phase.FADING_OUT;
+        Disposer.dispose(animator);
+        animator = new FadeInOutAnimator(false);
+        animator.resume();
+    }
+
+    private class FadeInOutAnimator extends Animator {
+        private boolean forward;
+
+        public FadeInOutAnimator(boolean forward) {
+            super("Action Hint Fade In/Out", 5, 100, false, forward);
+            this.forward = forward;
+        }
+
+        public void paintNow(int frame, int totalFrames, int cycle) {
+            if (forward && phase != Phase.FADING_IN
+                    || !forward && phase != Phase.FADING_OUT) return;
+            setAlpha(hintAlpha + (1 - hintAlpha) * (totalFrames - frame) / totalFrames);
+        }
+
+        public void paintCycleEnd() {
+            if (forward) {
+                showFinal();
+            } else {
+                close();
+            }
+        }
+
+        private void close() {
+            Disposer.dispose(this);
+        }
+
+    }
+
+
+    public enum Phase {FADING_IN, SHOWN, FADING_OUT, HIDDEN}
 }
