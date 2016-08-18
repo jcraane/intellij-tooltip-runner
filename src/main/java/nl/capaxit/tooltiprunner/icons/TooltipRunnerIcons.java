4e6a69e316ce837ce13bc73f990a41423cf9cdf7
jcraane <jcraane@gmail.com>
2016-08-18 18:33:37 +0200

    Started with icons.

---
+++ src/main/java/nl/capaxit/tooltiprunner/icons/TooltipRunnerIcons.java
@@ -0,0 +1,11 @@
+package nl.capaxit.tooltiprunner.icons;
+
+/**
+ * Created by jamiecraane on 18/08/16.
+ */
+public final class TooltipRunnerIcons {
+//    todo define new icons for tooltip runner.
+
+    private TooltipRunnerIcons() {
+    }
+}
